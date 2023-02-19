1990e729c5b33188e9fc23fc7185d5d2569bd367
adrgonza <105714518+adriiiiiiiii@users.noreply.github.com>
2023-02-19 21:33:54 +0100

    server and client working

--- client.c
+++ client.c
@@ -1,31 +1,43 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrgonza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 16:57:13 by adrgonza          #+#    #+#             */
+/*   Updated: 2023/02/19 21:18:32 by adrgonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-#include <stdlib.h>
 #include <signal.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int pid;
-    int i;
+	int	pid;
+	int	i;
 
-    if(argc != 3)
-        return(write(1, "\033[31mError:\nTry: ./.. 'pid' 'str'", 34));
-    i = 0;
-    while(argv[1][i])
-        if(argv[1][i++] < '0' || argv[1][i] > '9')
-            return(write(1, "\033[31mError: Invalid Pid...\n", 28));
-    pid = atoi(argv[1]);
-    while (*argv[2])
-    {
-        i = 8;
-	    while (i--)
-	    {
-		    if (*argv[2] >> i & 1)
-			    kill(pid, SIGUSR1);
-		    else
-			    kill(pid, SIGUSR2);
-		    usleep(200);
-	    }
-        argv[2]++;
-    }
+	if (argc != 3)
+		return (write(1, "\033[31mError:\nTry: ./.. 'pid' 'str'", 34));
+	i = 0;
+	while (argv[1][i])
+		if (argv[1][i++] < '0' || argv[1][i] > '9')
+			return (write(1, "\033[31mError: Invalid Pid...\n", 28));
+	i = 0;
+	while (argv[1][i] >= '0' && argv[1][i] <= '9')
+		pid = (argv[1][i++] - '0') + pid * 10;
+	while (*argv[2])
+	{
+		i = 8;
+		while (i--)
+		{
+			if (*argv[2] >> i & 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(50);
+		}
+		argv[2]++;
+	}
 }
--- server.c
+++ server.c
@@ -1,34 +1,50 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrgonza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 16:57:31 by adrgonza          #+#    #+#             */
+/*   Updated: 2023/02/19 21:24:52 by adrgonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <signal.h>
+
+void	ft_print_char(int signal)
+{
+	static int	i;
+	static int	chr;
+
+	if (signal == SIGUSR1)
+		chr = chr << 1 | 1;
+	else
+		chr = chr << 1;
+	if (++i == 8)
+	{
+		write(1, &chr, 1);
+		i = 0;
+		chr = 0;
+	}
+}
 
 int	ft_putnbr(int nb)
 {
 	if (nb > 9)
 		ft_putnbr(nb / 10);
 	nb = nb % 10 + '0';
-	write(1, &nb, 1);
+	return (write(1, &nb, 1));
 }
 
-void	ft_print_bites(int signal)
+int	main(void)
 {
-	static int chr;
-
-	if (signal = SIGUSR1)
-		chr = chr + (1 << )
-	else
-		chr = chr + (0 << );
-}
-
-int main()
-{
-    write(1, "\x1B[33m\n            Server running.\n\nPid number: ", 48);
-    ft_putnbr(getpid());
-    while(1)
-	{
-		signal(SIGUSR1, ft_print_bites);
-		signal(SIGUSR2, ft_print_bites);
-        pause();
-	}
-
+	write(1, "\033[0;32m            Server running\n\nPid number: ", 48);
+	ft_putnbr(getpid());
+	write(1, "\033[0m\n\n", 7);
+	signal(SIGUSR1, ft_print_char);
+	signal(SIGUSR2, ft_print_char);
+	while (1)
+		pause();
 }
